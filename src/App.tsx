9d071f3fb16115f485efec267c8917e52234b255
PureGero <puregero@gmail.com>
2021-10-20 11:45:54 +1000

    Add login button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import styled from 'styled-components';
 import AppLogo from './AppLogo';
 import ClickButton from './ClickButton';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+import firebase from 'firebase/app';
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+});
+
+const auth = firebase.auth();
+
+const authUiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ]
+};
 
 const AppContainer = styled.div`
   text-align: center;
@@ -19,21 +41,49 @@ const AppHeader = styled.header`
   color: white;
 `;
 
-interface AppProps {
-  
-}
+const App = () => {
+  const [isSignedIn, setIsSignedIn] = useState<boolean | null>(null);
 
-class App extends React.Component<AppProps> {
-  render() {  
+  useEffect(() => {
+    const unregisterAuthObserver = auth.onAuthStateChanged(user => {
+      setIsSignedIn(!!user);
+    });
+    return unregisterAuthObserver;
+  }, []);
+
+  if (isSignedIn === null) {
     return (
       <AppContainer>
         <AppHeader>
           <AppLogo src={logo} alt="logo" />
-          <ClickButton />
+          <p>Loading...</p>
         </AppHeader>
       </AppContainer>
     );
   }
+
+  if (!isSignedIn) {
+    return (
+      <AppContainer>
+        <AppHeader>
+          <h1>SRS Cards</h1>
+          <p>Please sign-in:</p>
+          <StyledFirebaseAuth uiConfig={authUiConfig} firebaseAuth={auth} />
+        </AppHeader>
+      </AppContainer>
+    );
+  }
+
+  return (
+    <AppContainer>
+      <AppHeader>
+        <p>Welcome {auth.currentUser && auth.currentUser.displayName}! You are now signed-in!</p>
+        <button onClick={() => auth.signOut()}>Sign-out</button>
+        <AppLogo src={logo} alt="logo" />
+        <ClickButton />
+      </AppHeader>
+    </AppContainer>
+  );
 }
 
 export default App;
