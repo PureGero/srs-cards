e93179936a297e18768c7bb1a7f770e5dc822e5c
PureGero <puregero@gmail.com>
2021-10-21 18:19:16 +1000

    Add personal click count

--- src/ClickButton.tsx
+++ src/ClickButton.tsx
@@ -16,8 +16,10 @@ const Warning = styled.div`
 const ClickButton = () => {
   const db = firebase.firestore();
   const counter = db.collection('counts').doc('counter');
+  const myCounter = db.collection('user_counts').doc(firebase.auth().currentUser?.uid);
 
   const [count, setCount] = useState(0);
+  const [myCount, setMyCount] = useState(0);
   const [message, setMessage] = useState('Loading...');
 
   useEffect(() => {
@@ -42,18 +44,40 @@ const ClickButton = () => {
     return unsubscribeCounter;
   });
 
+  useEffect(() => {
+    const unsubscribeMyCounter = myCounter.onSnapshot(doc => {
+      const data = doc.data();
+      if (!data) {
+        myCounter.set({});
+      } else {
+        setMyCount(data.value);
+      }
+    });
+
+    return unsubscribeMyCounter;
+  });
+
   const increment = async () => {
     try {
-      await counter.update({
-        value: firebase.firestore.FieldValue.increment(1)
-      });
+      await Promise.all([
+        counter.update({
+          value: firebase.firestore.FieldValue.increment(1)
+        }),
+        myCounter.update({
+          value: firebase.firestore.FieldValue.increment(1)
+        })
+      ]);
     } catch (e) {
       setMessage('Today\'s clicking limit has been reached, come back tomorrow!');
+      throw e;
     }
   };
 
   return (
     <div>
+      <p>
+        Your clicks: {myCount}
+      </p>
       <Button onClick={() => increment()}>
         {message ? message : `Clicks: ${count}`}
       </Button>
